d922125fedf54698368fa66a067cc2f34c90e17f
xucmihdik <xynqww@gmail.com>
2025-07-04 20:02:23 +0800

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,1 +1,10 @@
+import uuid
+from datetime import datetime, timedelta
 
+KEYS = {}
+
+def generate_key():
+    key = f"valid-{uuid.uuid4().hex[:10]}"
+    expires_at = (datetime.utcnow() + timedelta(hours=24)).isoformat()
+    KEYS[key] = expires_at
+    return key, expires_at
