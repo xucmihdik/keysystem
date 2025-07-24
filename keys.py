fd7ff1133f47f5b14357de5803d82b7b274102f1
xucmihdik <xynqww@gmail.com>
2025-07-24 15:55:32 +0800

    Update keys.py

--- keys.py
+++ keys.py
@@ -6,7 +6,7 @@ USED_IPS = {}
 
 def generate_key(ip):
     key = f"clark-{uuid.uuid4().hex[:12]}"
-    expires_at = datetime.utcnow() + timedelta(hours=24)
-    KEYS[key] = expires_at.isoformat()
+    expires_at = datetime.utcnow() + timedelta(hours=24)  # Set expiration to 24 hours from now
+    KEYS[key] = expires_at.isoformat()  # Store the expiration time in ISO format
     USED_IPS[ip] = key
     return key, expires_at.isoformat()
